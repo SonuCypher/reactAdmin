986d661b584399a21e49eade9f71ba3b5f91f0e4
Cypher890 <abdulsuhaib07@gmail.com>
2023-02-24 20:33:52 +0530

    created routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
+import React from "react";
+import { Link, Route, Routes,Outlet } from "react-router-dom";
+import Header from './components/Header/Header'
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
-  );
-}
-
+    <>
+    <nav className="navbar">
+      <div><Link >Name</Link></div>
+      <div><Link to='/about'>about</Link></div>
+      <div><Link >LOGIN/SIGNUP</Link></div>
+    </nav>
+    <Routes>
+      <Route path='/about' element={<Header />} />
+    </Routes>
+    <main>
+      <h1>jlhdsfhhahsldh</h1>
+    </main>
+{/* <Header /> */}
+    </>
+  )
+  }
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,14 @@
-import React from "react";
-import "./Header.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    // <div>
-    //   <form>
-    //     <span>
-    //     <label for="search">Search</label>
-    //       <input id="search" type="text" placeholder="place"></input>
-    //       <span className="caret"></span>
-    //     </span>
-    //     <span>
-    //       <input type="text" placeholder="product"></input>
-    //     </span>
-    //   </form>
-    // </div>
-    <div className="navbar">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Contact us</a>
-        </li>
-        <div className="search-box">
-          <label for='text'><i className="fas fa-search"></i></label>
-          <input
-            type="text"
-            className="input-search"
-            placeholder="Type to Search..."
-          />
-        </div>
-      </ul>
+    <div>
+      <h1>Header</h1>
+      <Link to='/'>HOME</Link>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
+
